af8488fe1b87549df2d0b69e5b8a6c8c3fa3e01c
hyj11123 <43002611+hyj11123@users.noreply.github.com>
2020-04-14 16:29:18 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Circular from "./components/Circular";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: false
+    };
+  }
+
+  // //render 전에 실행되는 라이프사이클 함수
+  // componentWillMount() {
+  //   console.log("[LIFECYCLE] WillMount");
+  // }
+
+  //render 후에 실행되는 라이프사이클함수
+  componentDidMount() {
+    //setTimeout(실행할 함수, 시간);
+    setTimeout(() => {
+      this.setState({
+        isLoading: true
+      });
+    }, 5000);
+  }
+
+  render() {
+    const { isLoading } = this.state;
+
+    return (
+      <div>{isLoading ? <div> 안녕하세요? 반가워요</div> : <Circular />}</div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Circular.js
@@ -0,0 +1,27 @@
+import React from "react";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+// React Hook
+const Circular = () => {
+  const [progress, setProgress] = React.useState(0);
+
+  React.useEffect(() => {
+    function trick() {
+      setProgress(oldProgress => (oldProgress >= 100 ? 0 : oldProgress + 1));
+    }
+
+    const timer = setInterval(trick, 20);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div>
+      <CircularProgress variant="determinate" value={progress} />
+    </div>
+  );
+};
+
+export default Circular;
